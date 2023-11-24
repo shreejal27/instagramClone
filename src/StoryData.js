246c1f891691535f00a2c7761aaed57f5ed2e7bf
Shreejal Maharjan <shreejal27@gmail.com>
2023-11-24 16:23:03 +0545

    added data to story and post

--- src/StoryData.js
+++ src/StoryData.js
@@ -1,34 +1,38 @@
 export default[
     {
-        userName: "juiceWrld",
-        userImg: "./images/juice.jpg "
+        userName: "itsdinksss",
+        userImg: "./images/story/d.jpg "
     },
     {
-        userName: "juiceWrld",
-        userImg: "./images/juice.jpg"
+        userName: "juicewrld999",
+        userImg: "./images/story/j.jpg"
     },
     {
-        userName: "juiceWrld",
-        userImg: "./images/juice.jpg"
+        userName: "leexon_xhrextha",
+        userImg: "./images/story/l.jpg"
     },
     {
-        userName: "juiceWrld",
-        userImg: "./images/juice.jpg"
+        userName: "_manogya",
+        userImg: "./images/story/m.jpg"
     },
     {
-        userName: "juiceWrld",
-        userImg: "./images/juice.jpg"
+        userName: "smmoto001",
+        userImg: "./images/story/s.jpg"
     },
     {
-        userName: "juiceWrld",
-        userImg: "./images/juice.jpg"
+        userName: "routineofnepalronb",
+        userImg: "./images/story/r.jpg"
     },
     {
-        userName: "juiceWrld",
-        userImg: "./images/juice.jpg"
+        userName: "anilamaharjan",
+        userImg: "./images/story/a.jpg"
     },
     {
-        userName: "juiceWrld",
-        userImg: "./images/juice.jpg"
-    }
+        userName: "fnatic",
+        userImg: "./images/story/f.jpg"
+    },
+    // {
+    //     userName: "__yaankaa",
+    //     userImg: "./images/story/ja.jpg"
+    // },
 ]
